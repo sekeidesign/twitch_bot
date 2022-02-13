18e0835b91e9e5d67563a819b07d7567aac258e2
Piergiorgio Gonni <pg@sekei.ca>
2022-02-13 14:37:41 -0500

    Add auto shoutout functionality

--- Shoutouts/shoutouts.js
+++ Shoutouts/shoutouts.js
@@ -1,34 +1,40 @@
 const shoutouts = {
-    tg_khalil: {
-        handle: "@tg_khalil",
-        message: "Do you feel those positive vibes? That's Khalil, a super zen co-working designer with an adorable kitty. Ask about Floki! 🐱"
-    },
-    aaoa_: {
-        handle: "@aaoa_",
-        message: "Aaoa is a Figma jedi and master designer ✨ he'll also do pull ups for channel points 👀"
-    },
-    amatrine: {
-        handle: "@amatrine",
-        message: "My gemstone 💎 Amatrine is the cutest action adventure gamer out there. She always brings that BEEFU!!!"
-    },
-    mannimoki: {
-        handle: "@mannimoki",
-        message: "The UX to my UI ✨ Manni is wonderful developer, designer, and 👁🐝Ⓜ️er"
-    },
-    mewtru: {
-        handle: "@mewtru",
-        message: ""
-    },
-    vapejuicejordan: {
-        handle: "@vapejuicejordan",
-        message: "DEEZ NUTS!"
-    },
-    trash_dev: {
-        handle: "@trash_dev",
-        message: ""
-    }
-}
+  tg_khalil: {
+    handle: "@tg_khalil",
+    message:
+      "Do you feel those positive vibes? That's Khalil, a super zen co-working designer with an adorable kitty. Ask about Floki! 🐱",
+  },
+  aaoa_: {
+    handle: "@aaoa_",
+    message:
+      "Aaoa is a Figma jedi and master designer ✨ he'll also do pull ups for channel points 👀",
+  },
+  amatrine: {
+    handle: "@amatrine",
+    message:
+      "My gemstone 💎 Amatrine is the cutest action adventure gamer out there. She always brings that BEEFU!!!",
+  },
+  mannimoki: {
+    handle: "@mannimoki",
+    message:
+      "The UX to my UI ✨ Manni is wonderful developer, designer, and 👁🐝Ⓜ️er",
+  },
+  mewtru: {
+    handle: "@mewtru",
+    message: "",
+  },
+  vapejuicejordan: {
+    handle: "@vapejuicejordan",
+    message: "DEEZ NUTS!",
+  },
+  trash_dev: {
+    handle: "@trash_dev",
+    message: "",
+  },
+  sekeidesign: {
+    handle: "@sekeidesign",
+    message: "test bish",
+  },
+};
 
-export default {
-    shoutouts
-}
+export default shoutouts;
