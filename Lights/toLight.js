7f19d89403518c3ac5f59f81794df087f33d1ce9
Piergiorgio Gonni <pg@sekei.ca>
2023-02-03 14:41:00 -0500

    Finalize Pomodoro UI

---
+++ Lights/toLight.js
@@ -0,0 +1,39 @@
+export default function toLight(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  const r = parseInt(result[1], 16) / 255;
+  const g = parseInt(result[2], 16) / 255;
+  const b = parseInt(result[3], 16) / 255;
+
+  var max = Math.max(r, g, b),
+    min = Math.min(r, g, b);
+  var h,
+    s,
+    l = (max + min) / 2;
+
+  if (max == min) {
+    h = s = 0; // achromatic
+  } else {
+    var d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+    switch (max) {
+      case r:
+        h = (g - b) / d + (g < b ? 6 : 0);
+        break;
+      case g:
+        h = (b - r) / d + 2;
+        break;
+      case b:
+        h = (r - g) / d + 4;
+        break;
+    }
+
+    h /= 6;
+  }
+
+  return {
+    hue: Math.round(h * 65535),
+    sat: Math.round(s * 254),
+    bri: Math.round(l * 254),
+  };
+}
